37b1aa66ba4a9dcac821d6c5dcb597bd95aff9ec
Kunnatam <ap0880800@gmail.com>
2022-03-28 01:13:29 +0700

    Update pub git protocol and update temp implementation for android

--- public_key_pinning_android/lib/public_key_pinning_android.dart
+++ public_key_pinning_android/lib/public_key_pinning_android.dart
@@ -1,13 +1,41 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:public_key_pinning_platform_interface/evaluation.dart';
+import 'package:public_key_pinning_platform_interface/public_key_pinning_platform_interface.dart';
 
-class PublicKeyPinningAndroid {
-  static const MethodChannel _channel = MethodChannel('public_key_pinning_android');
+const MethodChannel methodChannel = MethodChannel('plugins/public_key_pinning_android');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+/// An implementation of [PublicKeyPinningPlatform] for Android.
+class PublicKeyPinningAndroid extends PublicKeyPinningPlatform {
+  /// Registers this class as the default instance of [PublicKeyPinningPlatform].
+  static void registerWith() {
+    PublicKeyPinningPlatform.instance = PublicKeyPinningAndroid();
+  }
+
+  @override
+  Future<void> init(List<Evaluation> evaluations) async {
+    //! Bypass initialization untill native Android version is implemented.
+    return;
+
+    // final evs = {
+    //   "evaluations": evaluations,
+    // };
+    // return methodChannel.invokeMethod('init', evs);
+  }
+
+  @override
+  Future<bool> request({
+    required String url,
+    Map<String, dynamic>? headers,
+  }) async {
+    //! Bypass pin check untill native Android version is implemented.
+    return true;
+
+    // final arguments = {
+    //   'url': url,
+    //   'headers': headers,
+    // };
+    // return methodChannel.invokeMethod<bool>('request', arguments).then<bool>((bool? value) => value ?? false);
   }
 }
